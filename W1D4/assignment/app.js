9e1b7b0b70be3909daa75b2114fc436359f7ea90
Quy Tran <quytran288@gmail.com>
2017-10-06 08:57:47 -0500

    Write log using morgan

--- W1D4/assignment/app.js
+++ W1D4/assignment/app.js
@@ -7,11 +7,11 @@ const app = express();
 
 app.use('/resource', express.static(path.join(__dirname, 'public/assets')));
 
-const logStream = fs.createReadStream(path.join(__dirname, 'log/access.log'));
+const logStream = fs.createWriteStream(path.join(__dirname, 'log/access.log'), {flag: 'a'});
 app.use(morgan('combined', {stream: logStream}));
 
 app.get('/', (req, res) => {
-    res.send('Hello guys');
+    res.send('Hello MWA');
 })
 
 app.listen(3002);
